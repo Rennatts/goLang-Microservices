3af55c232108a0c46669166e1d26a985c4c7f42e
Renata Machado <16.renata@gmail.com>
2023-12-17 18:34:54 -0300

    basic http server

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
+
 	"github.com/renatama/microservices/handlers"
 )
 
@@ -13,12 +17,38 @@ import (
 
 func main() {
 	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+
 	hh := handlers.NewHello(l)
+	gh := handlers.NewGoodBye(l)
 
-	sm := http.NewServerMux()
+	sm := http.NewServeMux()
 	sm.Handle("/", hh)
+	sm.Handle("/goodbye", gh)
 
+	s := &http.Server{
+		Addr: ":9090",
+		Handler: sm,
+		IdleTimeout: 120*time.Second,
+		ReadTimeout: 1 * time.Second,
+		WriteTimeout: 1*time.Second,
+	}
 
-	http.ListenAndServe(":9090", sm)
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
 
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	sig := <-sigChan
+
+	l.Println("Received terminate, graceful shutdowm", sig)
+
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+
+	s.Shutdown(tc)
 }
